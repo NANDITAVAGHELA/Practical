db032843ce2aec8d9504136c42b7c46103cbc28d
NANDITA VAGHELA <161273656+NANDITAVAGHELA@users.noreply.github.com>
2024-03-23 21:01:10 +0530

    Add files via upload

---
+++ table.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+ int p;
+ int q=1;
+
+ cout<<"enter the number you want:";
+ cin>>p;
+
+
+ cout<<p<<" x "<<q<<" = "<<p*q<<endl;
+ q++;
+ cout<<p<<" x "<<q<<" = "<<p*q<<endl;
+ q++;
+ cout<<p<<" x "<<q<<" = "<<p*q<<endl;
+ q++;
+ cout<<p<<" x "<<q<<" = "<<p*q<<endl;
+ q++;
+ cout<<p<<" x "<<q<<" = "<<p*q<<endl;
+ q++;
+ cout<<p<<" x "<<q<<" = "<<p*q<<endl;
+ q++;
+ cout<<p<<" x "<<q<<" = "<<p*q<<endl;
+ q++;
+ cout<<p<<" x "<<q<<" = "<<p*q<<endl;
+ q++;
+ cout<<p<<" x "<<q<<" = "<<p*q<<endl;
+ q++;
+ cout<<p<<" x "<<q<<" = "<<p*q<<endl;
+ 
+  return 0;
+    
+}
