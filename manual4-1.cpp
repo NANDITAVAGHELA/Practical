fe779d5986e0fae4c9efa736bb67e1e29d61bd2a
NANDITA VAGHELA <161273656+NANDITAVAGHELA@users.noreply.github.com>
2024-03-31 22:10:48 +0530

    Add files via upload

---
+++ manual4-1.cpp
@@ -0,0 +1,30 @@
+ #include<iostream>
+using namespace std;
+int sort(int &a, int &b) {
+if(a<=b) {
+  return a;
+  return b;
+  } else {
+        int temp;
+        temp=b;
+        b=a;
+        a=temp;
+   return a;
+   return b;
+   }
+}
+   
+   int main() 
+   {
+   int a, b;
+   
+   cout << "enter two numbers:" << endl;
+   cin>>a>>b;
+
+   cout<<"Before swapping"<<"a="<<a<< " and "<<"b="<<b<<endl;
+
+   
+   sort(a, b) ;
+    cout<<"after swapping"<<"a="<<a<< " and "<<"b="<<b<<endl;
+   return 0;
+   }
